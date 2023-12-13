8b71e85cfe27d95d4f67dc353dc1d1fd213e954f
nachoddiaz <nachodiazutr@gmail.com>
2023-12-13 17:20:19 +0100

    update

---
+++ simple-Solidity/src/Counter.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Counter {
+    uint256 public number;
+    uint256 public number2;
+    uint256 public number3;
+    
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function increment() public {
+        number++;
+    }
+}
